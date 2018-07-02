0f5373ec57d4c73a04cc755531b808814ac35d7d
tdimnet <thomas.dimnet@gmail.com>
2018-07-02 17:20:36 +0200

    Start using functions as data

--- closures.playground/Contents.swift
+++ closures.playground/Contents.swift
@@ -1,1 +1,54 @@
+// Closures
+
+
+// Functions as Data
+func printString(_ string: String) {
+    print("Printing the string passed in as an argument: \(string)")
+}
+
+printString("Hi, my name is Thomas")
+
+let stringPinterFunction = printString
+stringPinterFunction("Hi, look at me!")
+
+
+func sum(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+let addTwoNumbers = sum
+addTwoNumbers(1, 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
