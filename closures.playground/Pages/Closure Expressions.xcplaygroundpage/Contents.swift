bbd6d3c98119a52bc56b4bc35f9894d87a2cd56f
tdimnet <thomas.dimnet@gmail.com>
2018-07-04 11:57:01 +0200

    Start the second part by adding a new playground page and a quick recap.

---
+++ closures.playground/Pages/Closure Expressions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+// Quick Recap
+func double(_ value: Int) -> Int {
+    return value * 2
+}
+
+let doubleFunction = double
+doubleFunction(2)
+
+let numbers = [1, 2, 3, 4]
+let doubledNumbers = numbers.map(doubleFunction)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
