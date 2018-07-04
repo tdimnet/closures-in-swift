67a9798c198251e1ca562715d14b9c3a4c885590
tdimnet <thomas.dimnet@gmail.com>
2018-07-04 12:09:40 +0200

    Work on Closure Expression Syntax.

--- closures.playground/Pages/Closure Expressions.xcplaygroundpage/Contents.swift
+++ closures.playground/Pages/Closure Expressions.xcplaygroundpage/Contents.swift
@@ -10,11 +10,18 @@ let numbers = [1, 2, 3, 4]
 let doubledNumbers = numbers.map(doubleFunction)
 
 
+// Closure Expression Syntax
 
+// Rule #1; Define the closure inline
+let tripledNumbers = numbers.map({(value: Int) -> Int in
+    return value * 3
+})
 
+// Rule #2: Inferring Type From Context
+numbers.map({ value in return value * 3 })
 
-
-
+// Rule #3: Implicit Returns from Single-Expression Closures
+numbers.map({ value in value * 3 })
 
 
 
